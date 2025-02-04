0fcfdef68a4f909b6814a985c6b6a681aed1628c
feona <feonateresafly@gmail.com>
2025-02-04 16:48:17 +0530

    home hero

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,13 @@
 // src/Components/BeyondBio.js
 import React from 'react';
 import Navbar from '../Components/Nav';
+import Hero from '../Components/Home/Hero';
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
-       
+      <Hero />
     </div>
   );
 };
